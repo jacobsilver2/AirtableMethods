e3a28e66a6c4a3aa62ff7f6c59fafddc2025ea02
Jacob <jacobsilver2@mac.com>
2021-12-04 22:51:40 -0500

    add find config

--- Event/AddEvent/addEvent.js
+++ Event/AddEvent/addEvent.js
@@ -1,93 +1,119 @@
-const Airtable = require('airtable');
-require('dotenv').config();
-const base = new Airtable({apiKey: 'keyY11TcpoTR646Fh'}).base('app4Eb0X39KtGToOS');
-const tynt = require('tynt');
-const inquirer = require('inquirer');
-const moment = require('moment');
-const questions = require('./addEventQuestions');
+const Airtable = require("airtable");
+require("dotenv").config({ path: require("find-config")(".env") });
+const base = new Airtable({ apiKey: `${process.env.API_KEY}` }).base(
+  "app4Eb0X39KtGToOS"
+);
+const tynt = require("tynt");
+const inquirer = require("inquirer");
+const moment = require("moment");
+const questions = require("./addEventQuestions");
 
-inquirer.prompt(questions.addEventQuestions).then(answers => {
-  createEvent(answers)
+inquirer.prompt(questions.addEventQuestions).then((answers) => {
+  createEvent(answers);
 });
 
 function createEvent(answers) {
-  const {name, date, time, status} = answers;
-  const formattedDate = moment.utc(`${date}, ${time}`, 'MMMD,YYYY, h:ma').format()
-  base('Events').create({
-    "Name": `${name}`,
-    "Status": `${status}`,
-    "Date": formattedDate
-  }, function(err, record) {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    console.log(tynt.Blink(`A new event has been created with the id ${record.getId()}.\nDo you want to add an act?`));
-    inquirer.prompt(questions.addActPromptQuestion).then(answers => {
-      if (answers.response === 'yes') {
-        inquirer.prompt(questions.addActQuestions).then(moreAnswers => {
-          checkIfActExistsAndAddToEvents(moreAnswers.email, record.get('Name'), record.getId());
-        })
-      }
-      return;
-    })
-  });
-}
-
-
-
-function createActandAddToEvent(eventId, name, email) {
-    base('Acts').create({
-      "Name": name,
-      "Email": email,
-      "Events": [
-        eventId
-      ]
-    }, function(err, record) {
+  const { name, date, time, status } = answers;
+  const formattedDate = moment
+    .utc(`${date}, ${time}`, "MMMD,YYYY, h:ma")
+    .format();
+  base("Events").create(
+    {
+      Name: `${name}`,
+      Status: `${status}`,
+      Date: formattedDate,
+    },
+    function (err, record) {
       if (err) {
         console.error(err);
         return;
       }
-      console.log('Succesfully created a new act, added it to the database, and linked to to the event.  Now fuck off');
-    });
+      console.log(
+        tynt.Blink(
+          `A new event has been created with the id ${record.getId()}.\nDo you want to add an act?`
+        )
+      );
+      inquirer.prompt(questions.addActPromptQuestion).then((answers) => {
+        if (answers.response === "yes") {
+          inquirer.prompt(questions.addActQuestions).then((moreAnswers) => {
+            checkIfActExistsAndAddToEvents(
+              moreAnswers.email,
+              record.get("Name"),
+              record.getId()
+            );
+          });
+        }
+        return;
+      });
+    }
+  );
+}
+
+function createActandAddToEvent(eventId, name, email) {
+  base("Acts").create(
+    {
+      Name: name,
+      Email: email,
+      Events: [eventId],
+    },
+    function (err, record) {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      console.log(
+        "Succesfully created a new act, added it to the database, and linked to to the event.  Now fuck off"
+      );
+    }
+  );
 }
 
 function addExistingActToEvent(eventId, actId) {
-  base('Events').update(eventId, {
-    "Act (link)": [
-      actId
-    ]
-  }, function(err, record) {
-    if (err) {
-      console.log(err)
-      return;
+  base("Events").update(
+    eventId,
+    {
+      "Act (link)": [actId],
+    },
+    function (err, record) {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      console.log(`Succesfully added act to event. Now fuck off.`);
     }
-    console.log(`Succesfully added act to event. Now fuck off.`)
-  })
+  );
 }
 
-function checkIfActExistsAndAddToEvents (email, name, eventId) {
-  base('Acts').select({
-    view: "Grid",
-    filterByFormula: `(AND({Email} = \"${email}\", {Name} = \"${name}\"))`
-  }).eachPage(function page(records, fetchNextPage) {
-    // This function (`page`) will get called for each page of records.
-    if (records.length === 0 ) {
-      console.log('No act with that name and email was found in the database. Creating a new record\n');
-      createActandAddToEvent(eventId, name, email);
-      return;
-    }
-
-    records.forEach(function(record) {
-      addExistingActToEvent(eventId, record.id)
+function checkIfActExistsAndAddToEvents(email, name, eventId) {
+  base("Acts")
+    .select({
+      view: "Grid",
+      filterByFormula: `(AND({Email} = \"${email}\", {Name} = \"${name}\"))`,
     })
-    // To fetch the next page of records, call `fetchNextPage`.
-    // If there are more records, `page` will get called again.
-    // If there are no more records, `done` will get called.
-    fetchNextPage();
-}, function done(err) {
-    if (err) { console.error(err); return; }
-});
+    .eachPage(
+      function page(records, fetchNextPage) {
+        // This function (`page`) will get called for each page of records.
+        if (records.length === 0) {
+          console.log(
+            "No act with that name and email was found in the database. Creating a new record\n"
+          );
+          createActandAddToEvent(eventId, name, email);
+          return;
+        }
+
+        records.forEach(function (record) {
+          addExistingActToEvent(eventId, record.id);
+        });
+        // To fetch the next page of records, call `fetchNextPage`.
+        // If there are more records, `page` will get called again.
+        // If there are no more records, `done` will get called.
+        fetchNextPage();
+      },
+      function done(err) {
+        if (err) {
+          console.error(err);
+          return;
+        }
+      }
+    );
 }
-
-
--- Event/findEvent/findEvent.js
+++ Event/findEvent/findEvent.js
@@ -1,19 +1,23 @@
-var Airtable = require('airtable');
-require('dotenv').config();
-var base = new Airtable({apiKey: `${process.env.API_KEY}`}).base('app4Eb0X39KtGToOS');
+var Airtable = require("airtable");
+require("dotenv").config({ path: require("find-config")(".env") });
+var base = new Airtable({ apiKey: `${process.env.API_KEY}` }).base(
+  "app4Eb0X39KtGToOS"
+);
 
-function findEvent (id) {
-  base('Events').find(id, function(err, record) {
-    if (err) { console.error(err); return; }
+function findEvent(id) {
+  base("Events").find(id, function (err, record) {
+    if (err) {
+      console.error(err);
+      return;
+    }
     getName(record);
-});
+  });
 }
 
-function getName (record) {
-  console.log(record.fields['Act Name (lookup)'][0])
+function getName(record) {
+  console.log(record.fields["Act Name (lookup)"][0]);
 }
 
 module.exports = {
-  findEvent
-}
-
+  findEvent,
+};
